73da70041fbed725ffcf231f46b95fbd0afde937
Tenzin Jorden <tenden96@gmail.com>
2021-12-17 08:52:56 -0800

    commit tree 120f7fcfb2dc2d8e9d857b67ab4c71ff055fac1d

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+
+const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => {  //currentValue is the value of the current element its on
+    return accumulator + currentValue;
+},0);   // 0 is the value accumulator is initially set to
+
+console.log(totalBatteries);
+
